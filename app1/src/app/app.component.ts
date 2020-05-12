9052295af01031c7f4fcf861f1edd5869f2384e2
Nassim REBOUH <nassim.rebouh@external.grtgaz.com>
2020-05-12 15:20:25 +0200

    first commit

--- app1/src/app/app.component.ts
+++ app1/src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
-import { assetUrl } from 'src/single-spa/asset-url';
+import { Component } from "@angular/core";
+import { assetUrl } from "src/single-spa/asset-url";
 
 @Component({
-  selector: 'app1-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app1-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app1';
-  yoshiUrl = assetUrl("yoshi.png");
+  title = "app1";
 }
